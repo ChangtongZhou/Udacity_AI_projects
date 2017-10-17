c7f1b4e438c11fc1ace93bd6210d5e973282c84a
ChangtongZhou <changtongzhou@gmail.com>
2017-10-16 23:37:59 -0700

    Update my model selectors

--- AIND_recognizer/my_model_selectors.py
+++ AIND_recognizer/my_model_selectors.py
@@ -71,100 +71,108 @@ class SelectorBIC(ModelSelector):
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
-
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on BIC scores
-        bics = []
         # Bayesian information criteria: BIC = -2 * logL + p * logN
         # L: likelihood of the fitted model
         # p: number of parameters
         # N: number of data points, which is component here
         try:
-            for component in self.n_components:
-                model = self.base_model(component)
+            best_bic_score = float("Inf")
+            best_model = None
+
+            for n in range(self.min_n_components, self.max_n_components + 1):
+                model = self.base_model(n)
+
                 logL = model.score(self.X, self.lengths)
-                p = component ** 2 + 2 * component * model.n_features - 1
-                bic = -2 * logL + p * math.log(component)
-                bics.append(bic)
-                states = self.n_components[np.argmin(bics)]
-                return self.base_model(states)
+                logN = np.log(len(self.X))
+
+                # p = = n^2 + 2*d*n - 1
+                d = model.n_features
+                p = n ** 2 + 2 * d * n - 1
+                bic_score = -2.0 * logL + p * logN
+                # score, model = self.bic_point(n)
+                if bic_score < best_bic_score:
+                    best_bic_score, best_model = bic_score, model
+            return best_model
+
         except:
             return self.base_model(self.n_constant)
 
-
 class SelectorDIC(ModelSelector):
     ''' select best model based on Discriminative Information Criterion
-
     Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
-    https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
     def select(self):
+        """ select the best model for self.this_word based on
+        DIC score for n between self.min_n_components and self.max_n_components
+        :return: GaussianHMM object
+        """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on DIC scores
-        # DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
-
-        best_dic = float("-inf")
-        best_model = None
         try:
-            for component in self.n_components:
-                model = self.base_model(component)
-                logL = model.score(self.X, self.lengths)
-                p = np.mean([model.score(self.hwords[word]) for word in self.words if word != self.this_word])
-                dic = logL - p
-                if dic > best_dic:
+            best_score = float("-Inf")
+            best_model = None
+            for n in range(self.min_n_components, self.max_n_components+1):
+                model = self.base_model(n)
+                scores = []
+                for word, (X, lengths) in self.hwords.items():
+                    if word != self.this_word:
+                        scores.append(model.score(X, lengths))
+                score = model.score(self.X, self.lengths) - np.mean(scores)
+                if score > best_score:
+                    best_score = score
                     best_model = model
-                    best_dic = dic
+            return best_model
 
-                return best_model
         except:
             return self.base_model(self.n_constant)
+
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
-    def cv_score(self, n):
-        """
-        Calculate the average log likelihood of cross-validation folds using the KFold class
-        :return: tuple of the mean likelihood and the model with the respective score
-        """
-        scores = []
-        split_method = KFold(n_splits=2)
-
-        for train_idx, test_idx in split_method.split(self.sequences):
-            self.X, self.lengths = combine_sequences(train_idx, self.sequences)
-
-            model = self.base_model(n)
-            X, l = combine_sequences(test_idx, self.sequences)
-
-            scores.append(model.score(X, l))
-        return np.mean(scores), model
-
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         # TODO implement model selection using CV
         # means = []
         # Break the training set into "folds"
-        try:
-            best_score = float("Inf")
-            best_model = None
-            for n in range(self.min_n_components, self.max_n_components+1):
-                score, model = self.cv_score(n)
-                if score < best_score:
-                    best_score = score
-                    best_model = model
-            return best_model
-        except:
-            return self.base_model(self.n_constant)
+        # if len(self.sequences) <= 1:
+        #     continue
+        # split_method = KFold(n_splits = min(len(self.sequences), 3))
+        best_logL = float("-inf")
 
+        for component in range(self.min_n_components, self.max_n_components+1):
+            if len(self.sequences) <= 1:
+                continue
+            split_method = KFold(n_splits = min(len(self.sequences), 3))
+            # model = self.base_model(component)
+            # Calculate model mean scores and fold them
+            # fold_scores = []
+            for train_idx, test_idx in split_method.split(self.sequences):
 
-  
+                # Training sequences split using KFold are recombined
+                trainX, trainLength = combine_sequences(train_idx, self.sequences)
+                # Get test sequences
+                testX, testLength = combine_sequences(test_idx, self.sequences)
+                # Record each model score
+                # fold_scores.append(model.score(testX, testLength))
+                try:
+                    model = GaussianHMM(n_components = component, covariance_type="diag", n_iter = 1000,
+                    random_state = self.random_state, verbose=False).fit(trainX, trainLength)
+                    logL = model.score(testX, testLength)
+
+                    if logL > best_logL:
+                        best_logL = logL
+                        best_num_components = component
+                        return self.base_model(best_num_components)
+                except:
+                    return self.base_model(self.n_constant)
+
+        
